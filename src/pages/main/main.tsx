d222f0e11fe35ee3044089548b37cdbf9517bf55
Saint <edukukabaridi89@gmail.com>
2026-07-08 00:34:19 +0000

    Make tab bar scrollable on desktop + all screen sizes

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -380,7 +380,7 @@ const AppWrapper = observer(() => {
                 >
                     <div>
                         {!isDesktop && left_tab_shadow && <span className='tabs-shadow tabs-shadow--left' />}{' '}
-                        <Tabs active_index={active_tab} className='main__tabs' onTabItemClick={handleTabChange} top>
+                        <Tabs active_index={active_tab} className='main__tabs' onTabItemClick={handleTabChange} top is_scrollable>
                             <div
                                 label={
                                     <>
